7222473b514b4afb323d1b6c35f5838da2134b19
Ruslan Leshchenko <leshchenkoruua@gmail.com>
2019-08-19 11:03:00 +0300

    Update Readme

--- app/src/main/java/com/leshchenko/circularlayoutmanager/MainActivity.kt
+++ app/src/main/java/com/leshchenko/circularlayoutmanager/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.leshchenko.circularlayoutmanagerlib.CircularRecyclerLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        circularRecycler.layoutManager = CircularRecyclerLayoutManager()
         circularRecycler.adapter = Adapter()
     }
 
